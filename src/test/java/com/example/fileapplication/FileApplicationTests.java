f0ef54dba4a7a67472d6680330dd87cdf1247119
Yağmur Gider <ygmrgider@gmail.com>
2021-02-08 20:52:21 +0300

    Initial commit

---
+++ src/test/java/com/example/fileapplication/FileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
