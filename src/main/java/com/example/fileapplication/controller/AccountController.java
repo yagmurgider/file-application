5bbc45c32943222e1a85041d0b2ec3f3c4b858af
Yağmur Gider <ygmrgider@gmail.com>
2021-02-12 15:55:32 +0300

    Swagger added for AccountController

--- src/main/java/com/example/fileapplication/controller/AccountController.java
+++ src/main/java/com/example/fileapplication/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.example.fileapplication.controller;
 
+import com.example.fileapplication.dto.FileDTO;
 import com.example.fileapplication.dto.LoginRequest;
 import com.example.fileapplication.dto.RegistrationRequest;
 import com.example.fileapplication.dto.TokenResponse;
@@ -7,6 +8,9 @@ import com.example.fileapplication.entity.User;
 import com.example.fileapplication.repository.UserRepository;
 import com.example.fileapplication.security.JwtTokenUtil;
 import com.example.fileapplication.service.impl.UserServiceImpl;
+import com.example.fileapplication.util.ApiPaths;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,7 +20,8 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/token")
+@RequestMapping(ApiPaths.AccountCtrl.CTRL)
+@Api(value = ApiPaths.AccountCtrl.CTRL, description = "Account APIs")
 public class AccountController {
 
     @Autowired
@@ -29,6 +34,7 @@ public class AccountController {
     private UserServiceImpl userService;
 
     @RequestMapping(method = RequestMethod.POST)
+    @ApiOperation(value = "Login", response = TokenResponse.class)
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) throws AuthenticationException {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
         final User user = userRepository.findByUsername(request.getUsername());
@@ -37,6 +43,7 @@ public class AccountController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ApiOperation(value = "Register", response = Boolean.class)
     public ResponseEntity<Boolean> register(@RequestBody RegistrationRequest registrationRequest) throws AuthenticationException {
         Boolean response = userService.register(registrationRequest);
         return ResponseEntity.ok(response);
